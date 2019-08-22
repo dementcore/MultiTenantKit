41aec93cbc71928c3ecfcee5fa02f5c74872b5c2
DementCore <53424423+dementcore@users.noreply.github.com>
2019-08-22 20:08:35 +0200

    Tenant Controller

---
+++ MVC/MultiTenantKit.Mvc/Controllers/TenantController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiTenantKit.Core.Models;
+
+namespace MultiTenantKit.Mvc.Controllers
+{
+    public class TenantController<TTenant> : Controller
+        where TTenant : ITenant
+    {
+
+        public TTenant Tenant
+        {
+            get
+            {
+                Core.Context.TenantContext<TTenant> tCtx = HttpContext.GetTenantContext<TTenant>();
+
+                TTenant tenant = (tCtx != null) ? tCtx.Tenant : default;
+
+                return tenant;
+            }
+        }
+
+        public string TenantUrlSlug
+        {
+            get
+            {
+                Core.Context.TenantContext<TTenant> tCtx = HttpContext.GetTenantContext<TTenant>();
+
+                string tenantSlug = (tCtx != null) ? tCtx.TenantUrlSlug : default;
+
+                return tenantSlug;
+            }
+        }
+    }
+}
